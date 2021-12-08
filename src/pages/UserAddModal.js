1af66a6f8a1060179e23df8e997743303be6def4
barjenbruchlaw <brian@kcpropertylaw.com>
2021-12-08 10:34:50 -0600

    added outside modal element

--- src/pages/UserAddModal.js
+++ src/pages/UserAddModal.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
-import { Container, Row, Col, Tabs, Tab, Button, Modal, InputGroup, FormControl, Form } from 'react-bootstrap'
-import PageNavBar from '../components/PageNavBar'
-import UserTable from './userTable'
-import './pages.css'
+import { Button, Modal, InputGroup, FormControl, Form } from 'react-bootstrap'
 
-const SystemMaintenance = () => {
+
+const UserAddModal = () => {
 
     const [show, setShow] = useState(false);
 
@@ -13,29 +11,7 @@ const SystemMaintenance = () => {
     const handleShow = () => setShow(true);
 
     return (
-        <div className='grid-container'>
-
-            <header className='page-header'><Container><Row>
-            <Col><h1>Docket Manager v. 0.3</h1></Col>
-            <Col className='text-end mt-auto'>Signed in as <a href='/' style={{ color: 'white' }}>Brian Barjenbruch</a></Col>
-            </Row></Container></header>
-
-            <div className='page-sidebar'>
-            <PageNavBar />
-            </div>
-
-            <main className='page-main'>
-            
-            <Tabs className='TabBar nav-justified'>
-            
-            <Tab eventKey='pm' title='Property Managers' className='p-3'>Property Managers:</Tab>
-            <Tab eventKey='owner' title='Property Owners' className='p-3'>Owners:</Tab>
-            <Tab eventKey='user' title='Users' className='p-3'>
-
-                <Button variant='dark' onClick={handleShow}>Add new user</Button>
-
-
-                <Modal show={show} onHide={handleClose} size='lg'>
+        <>
 
                     <Modal.Header closeButton>
                     <Modal.Title>Add new user</Modal.Title>
@@ -119,20 +95,9 @@ const SystemMaintenance = () => {
                         Save Changes
                     </Button>
                     </Modal.Footer>
-
-                </Modal>
-
-
-                <UserTable />
-
-            </Tab>
-            <Tab eventKey='jurisdiction' title='Jurisdictions' className='p-3'>Jurisdictions:</Tab>
-            <Tab eventKey='judge' title='Judges' className='p-3'>Judges:</Tab>
-
-            </Tabs>
-            </main>        
-        </div>
+            
+        </>
     )
 }
 
-export default SystemMaintenance
+export default UserAddModal
