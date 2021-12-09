c991314baf98b9b5752e6c3f4be9933bd211fdcc
barjenbruchlaw <brian@kcpropertylaw.com>
2021-12-09 16:25:06 -0600

    laptop optimizing

--- src/pages/UserAddModalContents.js
+++ src/pages/UserAddModalContents.js
@@ -9,7 +9,7 @@ const UserAddModalContents = () => {
                     
                     <Modal.Body>
 
-                    <InputGroup className="mb-3 py-1">
+                    <InputGroup className="mb-3">
                         <FormControl 
                         placeholder="First Name"
                         aria-label="firstName"
@@ -23,14 +23,14 @@ const UserAddModalContents = () => {
                         className='ml-3'
                         />
                     </InputGroup>
-                    <InputGroup className="mb-3 py-1">
+                    <InputGroup className="mb-3">
                         <FormControl
                         placeholder="Street Address"
                         aria-label="streetAddress"
                         aria-describedby="basic-addon1"
                         />
                     </InputGroup>
-                    <InputGroup className="mb-3 py-1">
+                    <InputGroup className="mb-3">
                         <FormControl
                         placeholder="City"
                         aria-label="city"
@@ -70,13 +70,13 @@ const UserAddModalContents = () => {
                     <Form.Check inline name='jobTitle' type='radio' id='isPMManager' label='Property Manager Supervisor' />
                     </Form>
 
-                    <Form className='py-3'>
+                    <Form>
                         <Form.Check inline name='requestEviction' type='switch' id='canRequestEviction' label='Request eviction?' />
                         <Form.Check inline name='requestDismiss' type='switch' id='canRequestDismiss' label='Request dismissal?' />
                         <Form.Check inline name='requestWrit' type='switch' id='canRequestWrit' label='Request writ?' />
                         <Form.Check inline name='UpdateCase' type='switch' id='canUpdateCase' label='Update Case?' />
                     </Form>
-                    <Form className='py-3'>
+                    <Form className='pb-3'>
                         <Form.Check inline name='CreatePM' type='switch' id='canCreatePM' label='Create Property Manager?' />
                         <Form.Check inline name='CreateOwner' type='switch' id='canCreateOwner' label='Create Owner?' />
                         <Form.Check inline name='CreateUser' type='switch' id='canCreateUser' label='Create User?' />
